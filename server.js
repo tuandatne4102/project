e18368f17234d7907c499f72cbfab7e24e92d966
tuandatne4102 <tuandatne4102@users.noreply.github.com>
2025-06-24 04:27:19 +0700

    ux-ui

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bcrypt = require("bcrypt");
 const session = require("express-session");
 
 // Set the view engine to EJS and the views directory 
-app.set('view engine', 'ejs'); app.set('views', path.join(__dirname, 'view')); 
+app.set('view engine', 'ejs'); app.set('views', path.join(__dirname, 'view'));
 
 // Serve static files from the 'public' directory 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -43,12 +43,12 @@ startApp();
 
 // Route dang ky
 app.get('/register', (req, res) => {
-    try {
-        res.render('register.ejs');
-    } catch (err) {
-        console.error("An error occurred", err);
-        res.status(500).send("An error occurred while fetching data");
-    }
+  try {
+    res.render('register.ejs');
+  } catch (err) {
+    console.error("An error occurred", err);
+    res.status(500).send("An error occurred while fetching data");
+  }
 });
 
 app.post("/register", async (req, res) => {
@@ -63,27 +63,27 @@ app.post("/register", async (req, res) => {
     const hashed = await bcrypt.hash(password, 10);
 
     const newUser = new User({
-        username,
-        email,
-        password: hashed
+      username,
+      email,
+      password: hashed
     });
 
     await newUser.save();
     res.redirect("/login");
   } catch (err) {
     console.log("Error register", err);
-    res.render("register", {error : "An error occurred"});
+    res.render("register", { error: "An error occurred" });
   }
 });
 
 // Route dang nhap
 app.get('/login', async (req, res) => {
-    try {
-        res.render('login.ejs');
-    } catch (err) {
-        console.error("An error occurred", err);
-        res.status(500).send("An error occurred while fetching data");
-    }
+  try {
+    res.render('login.ejs');
+  } catch (err) {
+    console.error("An error occurred", err);
+    res.status(500).send("An error occurred while fetching data");
+  }
 });
 
 app.post("/login", async (req, res) => {
@@ -106,20 +106,62 @@ app.post("/login", async (req, res) => {
 
 // Route dashboard
 app.get('/', async (req, res) => {
-    try {
-        res.render('dashboard.ejs');
-    } catch (err) {
-        console.error("An error occurred", err);
-        res.status(500).send("An error occurred while fetching data");
-    }
+  try {
+    res.render('dashboard.ejs');
+  } catch (err) {
+    console.error("An error occurred", err);
+    res.status(500).send("An error occurred while fetching data");
+  }
 });
 
 app.get('/quiz', async (req, res) => {
-    try {
-        res.render('quiz.ejs');
-    } catch (err) {
-        console.error("An error occurred", err);
-        res.status(500).send("An error occurred while fetching data");
-    }
+  try {
+    res.render('quiz.ejs');
+  } catch (err) {
+    console.error("An error occurred", err);
+    res.status(500).send("An error occurred while fetching data");
+  }
 });
 
+// app.get('/settings', async (req, res) => {
+//   try {
+//     res.render('settings.ejs');
+//   } catch (err) {
+//     console.error("An error occurred", err);
+//     res.status(500).send("An error occurred while fetching data");
+//   }
+// });
+
+//Route setting
+
+app.get('/settings', async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userId);
+    if (!user) return res.redirect("/login");
+
+    res.render('settings.ejs', { user });
+  } catch (err) {
+    console.error("❌ Lỗi khi tải settings:", err);
+    res.status(500).send("Lỗi khi tải cài đặt");
+  }
+});
+
+app.post('/settings', async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userId);
+    console.log(req.body.avatar);
+    if (!user) return res.redirect("/login");
+
+    user.setting.music = !!req.body.music;
+    user.setting.sound_effects = !!req.body.sound_effects;
+    user.setting.timer_per_question = !!req.body.timer_per_question;
+    user.setting.timer_whole_test = !!req.body.timer_whole_test;
+    user.avatar = req.body.avatar;
+
+    await user.save();
+    res.redirect("/settings");
+  } catch (err) {
+    console.error("❌ Lỗi khi cập nhật settings:", err);
+    res.status(500).send("Lỗi khi lưu cài đặt");
+  }
+});
